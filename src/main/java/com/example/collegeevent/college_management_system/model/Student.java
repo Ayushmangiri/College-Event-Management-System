b70439c358a977be3ace375a87d7030ee808a1a9
Ayushmangiri <ayushmangiri@gmail.com>
2025-04-11 17:11:53 +0530

    11/04/2025

--- src/main/java/com/example/collegeevent/college_management_system/model/Student.java
+++ src/main/java/com/example/collegeevent/college_management_system/model/Student.java
@@ -1,4 +1,21 @@
 package com.example.collegeevent.college_management_system.model;
 
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "students")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
 public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
 }
