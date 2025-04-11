211daa35cb47b3f1e15a6d5c18ce7515c12efe99
Ayushmangiri <ayushmangiri@gmail.com>
2025-04-11 17:16:26 +0530

    even modal worked

--- src/main/java/com/example/collegeevent/college_management_system/model/Event.java
+++ src/main/java/com/example/collegeevent/college_management_system/model/Event.java
@@ -1,4 +1,24 @@
 package com.example.collegeevent.college_management_system.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "events")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
 public class Event {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+
+    private String eventName;
+    private String eventDescription;
+    private  String eventData;
 }
