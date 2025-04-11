e4b7ea26b2d08a0d9ff8a8d485e934c73280df10
Ayushmangiri <ayushmangiri@gmail.com>
2025-04-11 17:20:37 +0530

    Registration model worked

--- src/main/java/com/example/collegeevent/college_management_system/model/Registration.java
+++ src/main/java/com/example/collegeevent/college_management_system/model/Registration.java
@@ -1,4 +1,26 @@
 package com.example.collegeevent.college_management_system.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "registration")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
 public class Registration {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "student_id")
+    private Student student;
+
+    @ManyToOne
+    @JoinColumn (name = "event_id")
+    private Event event;
 }
