123c811072139e43ee044973535a9f8c800ed1b0
Ayushmangiri <ayushmangiri@gmail.com>
2025-04-15 17:45:23 +0530

    Added

--- src/main/java/com/example/collegeevent/college_management_system/controller/StudentController.java
+++ src/main/java/com/example/collegeevent/college_management_system/controller/StudentController.java
@@ -4,10 +4,9 @@ package com.example.collegeevent.college_management_system.controller;
 import com.example.collegeevent.college_management_system.model.Student;
 import com.example.collegeevent.college_management_system.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/students")
@@ -16,10 +15,21 @@ public class StudentController {
     @Autowired
     private StudentService studentService;
 
-
       @PostMapping
 
         public Student createStudent(@RequestBody Student student){
           return  studentService.createStudent(student);
       }
+
+    @GetMapping
+    public List<Student> getAllStudents(){
+        return studentService.getAllStudents();
+    }
+
+
+
+    @PutMapping
+    public Student updateStudent(@RequestBody Student student,@PathVariable Long id){
+
+    }
 }
